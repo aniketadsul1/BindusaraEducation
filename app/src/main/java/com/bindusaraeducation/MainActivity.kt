543b3c2f4aea7bc612046b427f0e0f4fdaa0f721
Aniket Shahadeo Adsul <aniketadsul1@gmail.com>
2023-07-20 13:07:55 +0530

    Initial commit

---
+++ app/src/main/java/com/bindusaraeducation/MainActivity.kt
@@ -0,0 +1,105 @@
+package com.bindusaraeducation
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import android.os.Bundle
+import android.view.ViewGroup
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import com.bindusaraeducation.ui.theme.BindusaraEducationTheme
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if(isOnline(baseContext)){
+            setContent {
+                WebViewPage("https://bindusaraeducation.com/")
+            }        } else{
+            setContent {
+                WebViewPage("file:///android_asset/error.html")
+            }        }
+
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@SuppressLint("SetJavaScriptEnabled")
+@Composable
+fun WebViewPage(url: String){
+
+    val mutableStateTrigger = remember { mutableStateOf(false) }
+
+    AndroidView(
+        factory = {
+            WebView(it).apply {
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+                webViewClient = WebViewClient()
+
+                // to play video on a web view
+                settings.javaScriptEnabled = true
+
+                webViewClient = object : WebViewClient() {
+
+                    override fun onReceivedError(view: WebView?, request: WebResourceRequest?, error: WebResourceError?) {
+                        super.onReceivedError(view, request, error)
+                  /*      loadURL = if(isOnline(context)){
+                            "file:///android_asset/404.html" // general error message
+                        } else{
+                            "file:///android_asset/error.html" // no internet connection
+                        }
+                        mutableStateTrigger.value = true*/
+                    }
+                }
+
+                loadUrl(url)
+            }
+        }, update = {
+            it.loadUrl(url)
+        })
+
+}
+
+
+fun isOnline(context: Context): Boolean {
+    val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    // For 29 api or above
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork) ?: return false
+        return when {
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ->    true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) ->   true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ->   true
+            else ->     false
+        }
+    }
+    // For below 29 api
+    else {
+        @Suppress("DEPRECATION")
+        if (connectivityManager.activeNetworkInfo != null && connectivityManager.activeNetworkInfo!!.isConnectedOrConnecting) {
+            return true
+        }
+    }
+    return false
+}
